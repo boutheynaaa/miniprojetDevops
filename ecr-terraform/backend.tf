91397be79a1eea87820446ccdd125232ecdbec9d
boutheyna <boutheynahammami@gmail.com>
2026-01-09 18:39:54 +0100

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -7,7 +7,7 @@ terraform {
   }
  
   backend "s3" {
-    bucket = ml-terraform-state-ff024e5a"  # ← Changé ici
+    bucket = "ml-terraform-state-ff024e5a"
     key    = "ecr/terraform.tfstate"
     region = "us-east-1"
   }
