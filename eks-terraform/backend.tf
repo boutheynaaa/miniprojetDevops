f29daf1dccdcd489286340df9635952cdf27a34b
boutheyna <boutheynahammami@gmail.com>
2026-01-09 04:36:21 +0100

    Remove Terraform backend configuration

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,13 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket = "my-terraform-state-boutheyna"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1"
-    
-    # Disable features that require additional S3 permissions
-    # These are blocked in AWS Academy
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_credentials_validation = false
-  }
-}
+
