a02c2fdae02ab966f810fda63506ac098339bc65
boutheyna <boutheynahammami@gmail.com>
2026-01-09 03:29:38 +0100

    Change S3 key for Terraform backend

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ml-terraform-state-8749f2b4"
-    key    = "k8/terraform.tfstate"
+    key    = "s3-buckets/terraform.tfstate"
     region = "us-east-1"
   }
 }
