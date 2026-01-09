508b165494bc91de5c2588f0c0568c69fe219ece
boutheyna <boutheynahammami@gmail.com>
2026-01-09 05:34:22 +0100

    Update ecr-repo-main.tf

--- ecr-terraform/ecr-repo-main.tf
+++ ecr-terraform/ecr-repo-main.tf
@@ -4,17 +4,11 @@ provider "aws" {
 
 locals {
   services = [
-    "emailservice",
-    "checkoutservice",
-    "recommendationservice",
-    "frontend",
+    "productservice",
+    "orderservice",
+    "inventoryservice",
+    "gatewayservice",
     "paymentservice",
-    "productcatalogservice",
-    "cartservice",
-    "loadgenerator",
-    "currencyservice",
-    "shippingservice",
-    "adservice"
   ]
 }
 
