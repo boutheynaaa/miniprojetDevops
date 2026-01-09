f475c744009cd9823889f5b6aaa1f468be2a8b93
boutheyna <boutheynahammami@gmail.com>
2026-01-09 04:49:54 +0100

    Refactor EKS Terraform configuration
    Refactor EKS Terraform configuration to use existing IAM roles and update subnet references. Adjust node group scaling configuration and cluster naming.

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -1,154 +1,89 @@
-provider "aws" { 
-  region = "us-east-1" 
+# Provider
+provider "aws" {
+  region = "us-east-1"
 }
 
 # ----------------------------
-# Variables
+# IAM Roles existants (LabRole)
 # ----------------------------
-variable "node_group_name" {
-  description = "Name of the EKS node group"
-  type        = string
-  default     = "lab-eks-node-group"
+data "aws_iam_role" "master" {
+  name = "LabRole"
 }
 
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = "Cluster"
+data "aws_iam_role" "worker" {
+  name = "LabRole"
 }
 
 # ----------------------------
-# IAM Roles (LabRole)
+# VPC et Subnets PUBLICS
 # ----------------------------
-data "aws_iam_role" "master" { 
-  name = "LabRole" 
-}
-
-data "aws_iam_role" "worker" { 
-  name = "LabRole" 
-}
-
-# ----------------------------
-# VPC and Subnets
-# ----------------------------
-data "aws_vpc" "main" { 
+data "aws_vpc" "main" {
   filter {
     name   = "tag:Name"
-    values = ["Lab-VPC"] 
+    values = ["Lab-VPC"]
   }
 }
 
-data "aws_subnet" "subnet-1" { 
-  vpc_id = data.aws_vpc.main.id
-  filter { 
-    name   = "tag:Name" 
-    values = ["Public-Subnet-1"] 
-  } 
+# Utilisation des SUBNETS PUBLICS (ceux avec MapPublicIpOnLaunch = True)
+data "aws_subnet" "subnet-1" {
+  id = "subnet-09ffe973cb315171e"
 }
 
-data "aws_subnet" "subnet-2" { 
-  vpc_id = data.aws_vpc.main.id
-  filter { 
-    name   = "tag:Name" 
-    values = ["Public-Subnet-2"] 
-  } 
+data "aws_subnet" "subnet-2" {
+  id = "subnet-055f200dcd69d44c6"
 }
 
-data "aws_security_group" "selected" { 
+data "aws_security_group" "selected" {
   vpc_id = data.aws_vpc.main.id
-  filter { 
-    name   = "tag:Name" 
-    values = ["Lab-SG"] 
-  } 
+  filter {
+    name   = "group-name"
+    values = ["Lab-SG"]
+  }
 }
 
 # ----------------------------
 # EKS Cluster
 # ----------------------------
-resource "aws_eks_cluster" "eks" { 
-  name     = var.cluster_name
+resource "aws_eks_cluster" "eks" {
+  name     = "MelCluster"
   role_arn = data.aws_iam_role.master.arn
 
-  vpc_config { 
-    subnet_ids         = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id] 
-    security_group_ids = [data.aws_security_group.selected.id] 
+  vpc_config {
+    subnet_ids              = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
+    endpoint_public_access  = true
+    endpoint_private_access = true
+    security_group_ids      = [data.aws_security_group.selected.id]
   }
 
-  tags = { 
-    Name        = var.cluster_name
-    Environment = "dev" 
-    Terraform   = "true" 
+  tags = {
+    Name        = "MelCluster"
+    Environment = "dev"
+    Terraform   = "true"
   }
 }
 
 # ----------------------------
 # EKS Node Group
 # ----------------------------
-resource "aws_eks_node_group" "node-grp" { 
-  cluster_name    = aws_eks_cluster.eks.name 
+resource "aws_eks_node_group" "node-grp" {
+  cluster_name    = aws_eks_cluster.eks.name
   node_group_name = var.node_group_name
-  node_role_arn   = data.aws_iam_role.worker.arn 
-  subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id] 
-  capacity_type   = "ON_DEMAND" 
-  disk_size       = 20 
-  instance_types  = ["t2.medium"]
+  node_role_arn   = data.aws_iam_role.worker.arn
+  subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
+  
+  capacity_type  = "ON_DEMAND"
+  disk_size      = 20
+  instance_types = ["t2.medium"]
 
-  scaling_config { 
-    desired_size = 3 
-    max_size     = 10 
-    min_size     = 2 
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
-  update_config {
-    max_unavailable = 1
-  }
-
-  tags = { 
-    Name = var.node_group_name
-  }
-
-  # Ensure proper ordering
-  depends_on = [
-    aws_eks_cluster.eks
-  ]
-}
-
-# ----------------------------
-# OIDC Provider for ServiceAccount IAM Roles
-# ----------------------------
-data "tls_certificate" "oidc_thumbprint" { 
-  url = aws_eks_cluster.eks.identity[0].oidc[0].issuer 
-}
-
-resource "aws_iam_openid_connect_provider" "eks_oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.oidc_thumbprint.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  depends_on = [aws_eks_cluster.eks]
 
   tags = {
-    Name = "${var.cluster_name}-eks-oidc"
+    Name = "lab-eks-node-group"
   }
-}
-
-# ----------------------------
-# Outputs
-# ----------------------------
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = aws_eks_cluster.eks.endpoint
-}
-
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = aws_eks_cluster.eks.name
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ID attached to the EKS cluster"
-  value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
-}
-
-output "oidc_provider_arn" {
-  description = "ARN of the OIDC Provider"
-  value       = aws_iam_openid_connect_provider.eks_oidc.arn
 }
