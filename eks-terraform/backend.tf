2cd3144b60ac0c707b1cf95e92d92bbc310485e8
boutheyna <boutheynahammami@gmail.com>
2026-01-09 03:59:55 +0100

    Change Terraform S3 backend key to 'eks/terraform.tfstate'
    Updated the S3 key for the Terraform backend configuration.

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -1,7 +1,13 @@
 terraform {
   backend "s3" {
     bucket = "my-terraform-state-boutheyna"
-    key    = "k8/terraform.tfstate"
+    key    = "eks/terraform.tfstate"
     region = "us-east-1"
+    
+    # Disable features that require additional S3 permissions
+    # These are blocked in AWS Academy
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_credentials_validation = false
   }
 }
